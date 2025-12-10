ea84292f276e94a8bf866530f81fa5f2058b641c
TheLomaxProject <courtney.d.lomax@gmail.com>
2025-12-09 17:16:28 -0700

    Refactor comments and improve script clarity
    Updated comments and improved clarity in the script.

--- PowerShell_Install_SCRIPT.ps1
+++ PowerShell_Install_SCRIPT.ps1
@@ -2,7 +2,7 @@
 # PowerShell script to configure network FIRST, download & install the latest Chrome, import bookmarks, install TrinityG, and import certificates
 # You can change out the programs and add whatever programs you wish
 # Good for Baselining multiple PC/Laptops
-# Certificates section only needed for apps that require them i.e. ATAK
+# Certificates section only needed for apps that require them i.e. ATAK, ICE etc
 
 # Ensure the script runs as an administrator
 function Ensure-Admin {
@@ -149,5 +149,6 @@ function Import-CERCertificate {
 Import-CERCertificate "C:\[PATH]\[CERTIFICATE].cer" 
 
 Write-Host "Setup complete. Restart Chrome to apply changes."
+
 
 
